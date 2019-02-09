ab5e232ed233d13c947975b27dfa0105af601760
alexlav16 <alexandrelav16@gmail.com>
2019-02-08 21:26:07 -0500

    commit 4

--- FiguresGeometriques/platform.cs
+++ FiguresGeometriques/platform.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SFML.Graphics;
+using SFML.System;
 
 
 namespace FiguresGeometriques
@@ -11,9 +12,21 @@ namespace FiguresGeometriques
 	class Platform
 	{
 
-		int height = 5;
-		int width = 5;
-		public  RectangleShape platform = new RectangleShape();
+		private int height = 5;
+		private int width = 5;
+		private Vector2f position = new Vector2f(0, 0);
+		RectangleShape rect;
+
+		public Platform(Vector2f position)
+		{
+			this.position = position;
+			
+			rect = new RectangleShape();
+			rect.FillColor = Color.Red;
+			
+			
+		}
+		
 
 
 		
