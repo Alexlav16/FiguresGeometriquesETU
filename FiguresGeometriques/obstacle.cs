a8ca29eb4be40fa56be30b522d1c80988b330908
alexlav16 <alexandrelav16@gmail.com>
2019-02-09 21:13:56 -0500

    added obstacle class

---
+++ FiguresGeometriques/obstacle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Graphics;
+using SFML.System;
+
+
+namespace FiguresGeometriques
+{
+	class obstacle : Movable
+	{
+
+		int objdmg = 2;
+
+		public obstacle(Vector2f position, uint nbSides, Color color, float speed, int Objdmg)
+			: base(position, nbSides, color, speed)
+		{
+			objdmg = Objdmg;
+
+
+
+		}
+
+	}
+}
